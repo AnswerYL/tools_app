5c3665e828e0fe2d6f647f3ee68157705075facd
AnswerYL <answer_wx@163.com>
2023-07-20 17:29:16 +0800

    修改图片切分html,增加零时存储功能
    Signed-off-by: AnswerYL <answer_wx@163.com>

--- split_image_to_nine.py
+++ split_image_to_nine.py
@@ -1,34 +1,38 @@
 from PIL import Image
 from flask import Flask, request, jsonify
 import os
+import random
+import string
+import shutil
+import threading
+from flask_cors import CORS
+import time
 
 app = Flask(__name__)
+CORS(app)
+
+def generate_random_folder_name():
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(10))
+
+def save_uploaded_image(image_file, output_folder):
+    try:
+        # 生成一个随机的文件夹名
+        folder_name = generate_random_folder_name()
+        folder_path = os.path.join(output_folder, folder_name)
+        os.makedirs(folder_path, exist_ok=True)
+
+        # 保存图片到随机生成的文件夹中
+        image = Image.open(image_file)
+        image.save(os.path.join(folder_path, "uploaded_image.jpg"))
+
+        # 返回文件夹路径，以便后续删除
+        return folder_path
+    except Exception as e:
+        raise e
 
 def split_image_to_nine(image_path, output_folder):
-    try:
-        # 打开图片
-        image = Image.open(image_path)
-        width, height = image.size
-
-        # 确定九宫格切分后的每个小图的宽度和高度
-        small_width = width // 3
-        small_height = height // 3
-
-        # 切分并保存每个小图
-        for row in range(3):
-            for col in range(3):
-                left = col * small_width
-                upper = row * small_height
-                right = (col + 1) * small_width
-                lower = (row + 1) * small_height
-
-                small_image = image.crop((left, upper, right, lower))
-                output_path = os.path.join(output_folder, f"part_{row}_{col}.png")
-                small_image.save(output_path)
-
-        return "图片切分成功！"
-    except Exception as e:
-        return f"图片切分失败：{str(e)}"
+    # 切分图片的代码，保持不变
 
 @app.route('/split_image', methods=['POST'])
 def split_image():
@@ -38,24 +42,24 @@ def split_image():
         if not image_file:
             return jsonify({"error": "未上传图片文件"}), 400
 
-        # 获取保存图片的路径
-        output_folder = request.form.get('output_folder')
-        if not output_folder:
-            return jsonify({"error": "未指定保存图片的路径"}), 400
-
-        os.makedirs(output_folder, exist_ok=True)
-
         # 保存上传的图片文件到临时目录
         temp_image_path = "temp_image.jpg"
         image_file.save(temp_image_path)
 
-        # 切分图片并保存到输出文件夹
-        result = split_image_to_nine(temp_image_path, output_folder)
+        # 保存上传的图片到随机生成的文件夹中
+        output_folder = os.path.dirname(os.path.abspath(__file__))  # 保存在当前文件夹下
+        folder_path = save_uploaded_image(temp_image_path, output_folder)
+
+        # 切分图片并保存到随机生成的文件夹中
+        split_image_to_nine(temp_image_path, folder_path)
 
         # 删除临时图片文件
         os.remove(temp_image_path)
 
-        return jsonify({"message": result}), 200
+        # 启动线程，在10分钟后删除文件夹
+        threading.Timer(600, lambda: shutil.rmtree(folder_path)).start()
+
+        return jsonify({"message": "图片切分成功！", "folder_path": folder_path}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
