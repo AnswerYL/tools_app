f5eb7893db99c52e7aab21ff0b3a37d40914519a
AnswerYL <answer_wx@163.com>
2023-07-20 17:40:33 +0800

    修改图片切分html,增加零时存储功能
    Signed-off-by: AnswerYL <answer_wx@163.com>

--- split_image_to_nine.py
+++ split_image_to_nine.py
@@ -17,46 +17,64 @@ def generate_random_folder_name():
 
 def save_uploaded_image(image_file, output_folder):
     try:
-        # 生成一个随机的文件夹名
+        # Generate a random folder name
         folder_name = generate_random_folder_name()
         folder_path = os.path.join(output_folder, folder_name)
         os.makedirs(folder_path, exist_ok=True)
 
-        # 保存图片到随机生成的文件夹中
+        # Save the uploaded image to the randomly generated folder
         image = Image.open(image_file)
+        image = image.convert('RGB')  # Convert RGBA to RGB
         image.save(os.path.join(folder_path, "uploaded_image.jpg"))
 
-        # 返回文件夹路径，以便后续删除
+        # Return the folder path for later use
         return folder_path
     except Exception as e:
         raise e
 
 def split_image_to_nine(image_path, output_folder):
-    # 切分图片的代码，保持不变
+    # Image splitting code, unchanged
+    try:
+        # 打开图片
+        image = Image.open(image_path)
+        width, height = image.size
+
+        # 确定九宫格切分后的每个小图的宽度和高度
+        small_width = width // 3
+        small_height = height // 3
+
+        # 切分并保存每个小图
+        for row in range(3):
+            for col in range(3):
+                left = col * small_width
+                upper = row * small_height
+                right = (col + 1) * small_width
+                lower = (row + 1) * small_height
+
+                small_image = image.crop((left, upper, right, lower))
+                output_path = os.path.join(output_folder, f"part_{row}_{col}.png")
+                small_image.save(output_path)
+
+        return "图片切分成功！"
+    except Exception as e:
+        return f"图片切分失败：{str(e)}"
 
 @app.route('/split_image', methods=['POST'])
 def split_image():
     try:
-        # 获取上传的图片文件
+        # Get the uploaded image file
         image_file = request.files.get('image')
         if not image_file:
             return jsonify({"error": "未上传图片文件"}), 400
 
-        # 保存上传的图片文件到临时目录
-        temp_image_path = "temp_image.jpg"
-        image_file.save(temp_image_path)
+        # Save the uploaded image to the current folder
+        output_folder = os.path.dirname(os.path.abspath(__file__))  # Save in the current folder
+        folder_path = save_uploaded_image(image_file, output_folder)
 
-        # 保存上传的图片到随机生成的文件夹中
-        output_folder = os.path.dirname(os.path.abspath(__file__))  # 保存在当前文件夹下
-        folder_path = save_uploaded_image(temp_image_path, output_folder)
+        # Split the image and save it to the randomly generated folder
+        split_image_to_nine(image_file, folder_path)
 
-        # 切分图片并保存到随机生成的文件夹中
-        split_image_to_nine(temp_image_path, folder_path)
-
-        # 删除临时图片文件
-        os.remove(temp_image_path)
-
-        # 启动线程，在10分钟后删除文件夹
+        # Start a thread to delete the folder after 10 minutes
         threading.Timer(600, lambda: shutil.rmtree(folder_path)).start()
 
         return jsonify({"message": "图片切分成功！", "folder_path": folder_path}), 200
